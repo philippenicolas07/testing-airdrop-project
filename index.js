db41dcdd4df101508a7c069e1c4420f84db78824
philippenicolas07 <107654803+philippenicolas07@users.noreply.github.com>
2022-07-21 08:57:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL,
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = wallet._keypair.publicKey
+const secretKey = wallet._keypair.secretKey
+
+
